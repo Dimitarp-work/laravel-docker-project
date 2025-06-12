f54be271136d7012109166a5c7580bd0ff338785
Dimitar Parvanov <parv0006@hz.nl>
2025-06-12 23:48:33 +0200

    Fix route conflict and redirect root to /notes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::resource('notes', NoteController::class);
 Route::redirect('/', '/notes');
-Route::resource('notes', App\Http\Controllers\NoteController::class);
+
+Route::resource('notes', NoteController::class);
