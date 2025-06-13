eb54ead6b550c45362f5dd7b5401359c0258fde4
Dimitar Parvanov <parv0006@hz.nl>
2025-06-13 15:23:57 +0200

    Test route for Heroku root

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\NoteController;
 
+//
 Route::get('/', function () {
     return '✅ Laravel is working!';
 });
