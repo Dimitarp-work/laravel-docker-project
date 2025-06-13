fdc061bb2164cad1ebd7ffe39246d97121535d88
Dimitar Parvanov <parv0006@hz.nl>
2025-06-13 14:50:27 +0200

    Add root route for testing

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/migrate-now', function () {
 
 Route::redirect('/', '/notes');
 
+Route::get('/', function () {
+    return 'Laravel is working!';
+});
 Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
